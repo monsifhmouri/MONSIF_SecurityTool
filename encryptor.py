87d053cccac8dcbaf974a24d1d06c9ec7b06ba5b
MONSIF HMOURI <71984109+monsifhmouri@users.noreply.github.com>
2025-02-20 19:06:16 +0100

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -5,7 +5,7 @@ from rich.console import Console
 console = Console()
 
 # استخدم كلمة مرور قوية هنا
-PASSWORD_HASH = bcrypt.hashpw(b"mamakorra1990", bcrypt.gensalt())
+PASSWORD_HASH = bcrypt.hashpw(b"ur password", bcrypt.gensalt())
 
 def verify_password():
     password = getpass.getpass("[bold cyan]أدخل كلمة المرور: [/bold cyan]").encode('utf-8')
